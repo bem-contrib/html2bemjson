a0b429d32969019905a96705ddc47f8803414ccc
Vladimir Grinenko <i@tadatuta.com>
2017-07-15 10:30:13 +0100

    Update bem-naming, switch to tag as block mode

--- index.js
+++ index.js
@@ -1,7 +1,124 @@
 var htmlparser = require('htmlparser2'),
-    bemNaming = require('bem-naming'),
+    bemNaming = require('@bem/naming'),
     stringifyObj = require('stringify-object'),
     vm = require('vm');
+
+var HTML_TAGS = {
+    a : true,
+    abbr : true,
+    address : true,
+    area : true,
+    article : true,
+    aside : true,
+    audio : true,
+    b : true,
+    base : true,
+    bdi : true,
+    bdo : true,
+    blockquote : true,
+    body : true,
+    br : true,
+    button : true,
+    canvas : true,
+    caption : true,
+    cite : true,
+    code : true,
+    col : true,
+    colgroup : true,
+    data : true,
+    datalist : true,
+    dd : true,
+    del : true,
+    details : true,
+    dfn : true,
+    dialog : true,
+    div : true,
+    dl : true,
+    dt : true,
+    em : true,
+    embed : true,
+    fieldset : true,
+    figcaption : true,
+    figure : true,
+    footer : true,
+    form : true,
+    h1 : true,
+    h2 : true,
+    h3 : true,
+    h4 : true,
+    h5 : true,
+    h6 : true,
+    head : true,
+    header : true,
+    hgroup : true,
+    hr : true,
+    html : true,
+    i : true,
+    iframe : true,
+    img : true,
+    input : true,
+    ins : true,
+    kbd : true,
+    keygen : true,
+    label : true,
+    legend : true,
+    li : true,
+    link : true,
+    main : true,
+    map : true,
+    mark : true,
+    menu : true,
+    menuitem : true,
+    meta : true,
+    meter : true,
+    nav : true,
+    noscript : true,
+    object : true,
+    ol : true,
+    optgroup : true,
+    option : true,
+    output : true,
+    p : true,
+    param : true,
+    pre : true,
+    progress : true,
+    q : true,
+    rb : true,
+    rp : true,
+    rt : true,
+    rtc : true,
+    ruby : true,
+    s : true,
+    samp : true,
+    script : true,
+    section : true,
+    select : true,
+    small : true,
+    source : true,
+    span : true,
+    strong : true,
+    style : true,
+    sub : true,
+    summary : true,
+    sup : true,
+    table : true,
+    tbody : true,
+    td : true,
+    template : true,
+    textarea : true,
+    tfoot : true,
+    th : true,
+    thead : true,
+    time : true,
+    title : true,
+    tr : true,
+    track : true,
+    u : true,
+    ul : true,
+    var : true,
+    video : true,
+    wbr : true
+};
 
 function isEmpty(obj) {
     for(var key in obj) {
@@ -47,8 +164,10 @@ var convert = function(html, opts) {
         },
         onopentag: function(tag, attrs) {
             var bemjsonNode = {},
-                classes = attrs.class && attrs.class.split(/\s/),
+                classes = ((attrs.class || '') + (attrs.mix || '')).split(/\s/).filter(Boolean),
                 visitedBlocksOnCurrentNode = [];
+
+            delete attrs.mix;
 
             function onEntity(entity) {
                 if (typeof entity === 'string') {
@@ -59,16 +178,14 @@ var convert = function(html, opts) {
                 var blockName = entity.block,
                     modFieldName = entity.elem ? 'elemMods' : 'mods';
 
-                naming.isBlock(entity) && visitedBlocksOnCurrentNode.push(blockName);
+                entity.type === 'block' && visitedBlocksOnCurrentNode.push(blockName);
 
-                if (naming.isBlockMod(entity) && visitedBlocksOnCurrentNode.indexOf(blockName) < 0) {
+                if (entity.type === 'blockMod' && visitedBlocksOnCurrentNode.indexOf(blockName) < 0) {
                     onEntity({ block: blockName });
                 }
 
                 if (!bemjsonNode.block) {
-                    Object.keys(entity).forEach(function(key) {
-                        bemjsonNode[key] = entity[key];
-                    });
+                    Object.assign(bemjsonNode, entity.valueOf());
 
                     return;
                 }
@@ -106,22 +223,27 @@ var convert = function(html, opts) {
                         }
                     }
 
-                    bemjsonNode.mix = (bemjsonNode.mix || []).concat(entity);
+                    bemjsonNode.mix = (bemjsonNode.mix || []).concat(entity.valueOf());
                 }
             }
 
+            var isHtmlTag = HTML_TAGS.hasOwnProperty(tag);
+
+            if (!isHtmlTag) {
+                (classes || (classes = [])).unshift(tag);
+                tag = 'div';
+            }
+
             if (classes && classes.length) {
-                var entities = classes.map(function(cls) {
-                        var entity = naming.parse(cls);
+                classes.forEach(function(cls) {
+                    var entity = naming.parse(cls);
 
-                        if (!entity && opts.preserveWrongClasses) {
-                            return cls;
-                        }
+                    if (!entity && opts.preserveWrongClasses) {
+                        cls && onEntity(cls);
+                    }
 
-                        return entity;
-                    });
-
-                entities.forEach(onEntity);
+                    onEntity(entity);
+                });
 
                 if (!bemjsonNode.block && bemjsonNode.mix && bemjsonNode.mix.length) {
 
@@ -171,7 +293,21 @@ var convert = function(html, opts) {
                 bemjsonNode.tag = tag;
             }
 
-            if (!isEmpty(attrs)) bemjsonNode.attrs = attrs;
+            if (!isEmpty(attrs)) {
+                var mods = {};
+
+                for (var key in attrs) {
+                    attrs[key] === '' && (attrs[key] = true);
+
+                    if (key[0] === '_') {
+                        mods[key.substr(1)] = attrs[key];
+                        delete attrs[key];
+                    }
+                }
+
+                bemjsonNode[bemjsonNode.elem ? 'elemMods' : 'mods'] = mods;
+                isHtmlTag ? bemjsonNode.attrs = attrs : Object.assign(bemjsonNode, attrs);
+            }
 
             if (isEmpty(bemjsonNode)) bemjsonNode = { content: '' };
 
@@ -225,6 +361,9 @@ var convert = function(html, opts) {
             last.content || (last.content = []);
             last.content.push(text);
         }
+    }, {
+        lowerCaseTags: false,
+        lowerCaseAttributeNames: false
     });
 
     parser.write(html);
